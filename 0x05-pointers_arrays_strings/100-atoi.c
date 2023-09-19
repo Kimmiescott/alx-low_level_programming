1816d444d458aa40a1bec5d2c1e966be283711a3
Kimmiescott <kimscott530@gmail.com>
2023-09-19 18:43:31 +0300

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -43,12 +43,14 @@ int idx_num_starts(char *s)
  */					
 int find_sign(char *s)
 {
-	int negatives = 0, i = 0, sign = 1;		
+	int negatives = 0, i = 0, sign = 1;
+
 	while (i < (idx_num_starts(s)))
 	{
 		if (s[i++] == '-')
                 negatives++;
 	}
+
 	if (negatives % 2 != 0)
 	sign = -1;
 										
