f8353650974b97250a5a4f96d9817163a4e3ec75
Kimmiescott <kimscott530@gmail.com>
2023-09-22 12:47:12 +0300

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -22,28 +22,7 @@ char *_strcat(char *dest, char *src)
 		j++;
 	}
 		dest[i] = '\0';
-
+		
 		return (dest);
 }
 
-/* another method using pointer notation
- *
- * char *_strcat(char *dest, char *src)
- * {
- * int i = 0;
- * int j = 0;
- *
- * while (*(dest + i))
- * i++;
- *
- * while (*(src + j))
- * {
- * *(dest+i) = *(src+j);
- * i++;
- * j++;
- * }
- *(dest+i) = '\0';
- *
- * return (dest);
- * }
- */
