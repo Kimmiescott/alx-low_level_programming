43966896781eba364b8af088a928c4df22f5cf81
Kimmiescott <kimscott530@gmail.com>
2023-09-28 17:20:21 +0300

    0x08-recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,18 +1,22 @@
 #include "main.h"
 
 /**
- * _puts_recursion - print string with new line
- * @s: string to print
+ * _pow_recursion - raise x to power y
+ * @x: number
+ * @y: power to raise to
+ * Return: power of a number, -1 if power less than 0
  */
 
-void _puts_recursion(char *s)
+int _pow_recursion(int x, int y)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-		_putchar(*s);
-		s++;
-		_puts_recursion(s);
+	if (y < 0)
+		return (-1);
+
+	if (x == 0)
+		return (0);
+
+	if (x == 1 || y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
 }
