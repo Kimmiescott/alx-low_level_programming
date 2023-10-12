0f9cb3e8e2ab2be6bd1e0c75099866da3f2d83e0
Kimmiescott <kimscott530@gmail.com>
2023-10-12 17:21:37 +0300

    0x10-variadic_functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -59,21 +59,26 @@ void print_all(const char * const format, ...)
 
 	datatype choice[] = { {'c', print_char},
 			      {'i', print_int},
-			      {'f', print_float},									      	      {'s', print_string},										      {'\0', NULL} };
+			      {'f', print_float}
+			      {'s', print_string},
+			      {'\0', NULL} };
 	/* iterate format; if datatype matched, access function via struct */
 	va_start(valist, format);
 	while (format != NULL && format[j] != '\0')
 	{
 		i = 0
-		while (choice[i].letter != '\0')									{
-																if (choice[i].letter == format[j])
-																{
-		     															printf("%s", separator);										choice[i].func(valist); /*access va_arg later*/
-			        separator = ", ";										}
-																i++;
-																												}
-															j++;
-														}
+		while (choice[i].letter != '\0')
+		{
+			if (choice[i].letter == format[j])
+			{
+		     	printf("%s", separator);										
+			choice[i].func(valist); /*access va_arg later*/
+		        separator = ", ";										
+			}
+			i++;																						
+		}
+		j++;
+	}
 	va_end(valist);
 	printf("\n");
 }
