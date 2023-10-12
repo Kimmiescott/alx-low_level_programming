5ad79206be8077049972331d88e621078d165c48
Kimmiescott <kimscott530@gmail.com>
2023-10-12 19:25:53 +0300

    0x10-variadic_functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,84 +1,106 @@
-#include <stdarg.h>
 #include <stdio.h>
-#include "variadic_functions.h"
+#include <stdarg.h>
+
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+void print_all(const char * const format, ...);
 
 /**
- * print_char - prints char
- * @valist: valist
+ * print_char - prints a character
+ * @arg: list of args pointing to the char to be printed
  */
-void print_char(va_list valist)
+void print_char(va_list arg)
 {
-	printf("%c", va_arg(valist, int));
+	char c;
+
+	c = va_arg(arg, int);
+	printf("%c", c);
 }
 
 /**
- * print_int - prints int
- * @valist: valist
+ * print_int - prints an int
+ * @arg: list of args pointing to the int to be printed
  */
-void print_int(va_list valist)
+void print_int(va_list arg)
 {
-	printf("%d", va_arg(valist, int));
+	int i;
+
+	i = va_arg(arg, int);
+	printf("%d", i);
 }
 
 /**
- * print_float - prints float
- * @valist: valist
+ * print_float - prints a float int
+ * @arg: list of args pointing to the float to be printed
  */
-void print_float(va_list valist)
+void print_float(va_list arg)
 {
-	printf("%f", va_arg(valist, double));
+	float f;
+
+	f = va_arg(arg, double);
+	printf("%f", f);
 }
 
 /**
- * print_string - prints string
- * @valist: valist
+ * print_string - prints a string
+ * @arg: list of args pointing to the string to be printed
  */
-void print_string(va_list valist)
+void print_string(va_list arg)
 {
 	char *s;
 
-	s = va_arg(valist, char *);
+	s = va_arg(arg, char *);
 
 	if (s == NULL)
 	{
 		printf("(nil)");
 		return;
 	}
+
 	printf("%s", s);
 }
 
 /**
- * print_all - print varying input of ints, chars, floats, and strings
- * @format: an array of chars signifying which data type to print
+ * print_all - Prints anything, followed by a new line.
+ * @format: A string of characters representing the argument types.
+ * @...: A variable number of arguments to be printed.
+ * Description: Any argument not of type char, int, float,
+ * or char * is ignored.
  */
 void print_all(const char * const format, ...)
 {
+	va_list args;
+	int i = 0, j = 0;
 	char *separator = "";
-	int i, j = 0;
-	va_list valist;
+	printer_t funcs[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
 
-	datatype choice[] = { {'c', print_char},
-			      {'i', print_int},
-			      {'f', print_float}
-			      {'s', print_string},
-			      {'\0', NULL} };
-	/* iterate format; if datatype matched, access function via struct */
-	va_start(valist, format);
-	while (format != NULL && format[j] != '\0')
+	va_start(args, format);
+
+	while (format && (*(format + i)))
 	{
-		i = 0
-		while (choice[i].letter != '\0')
+		j = 0;
+
+		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
+			j++;
+
+		if (j < 4)
 		{
-			if (choice[i].letter == format[j])
-			{
-		     	printf("%s", separator);										
-			choice[i].func(valist); /*access va_arg later*/
-		        separator = ", ";										
-			}
-			i++;																						
+			printf("%s", separator);
+			funcs[j].print(args);
+			separator = ", ";
 		}
-		j++;
+
+		i++;
 	}
-	va_end(valist);
+
 	printf("\n");
+
+	va_end(args);
 }
